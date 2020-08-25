7a03ddd89cb0506f9bcf3fce2a7bcff1a02058f2
MarcusPianco <marcusjrdev@gmail.com>
2020-08-25 18:35:40 -0300

    configuration: add topics init on docker-compose

--- dist/ClientKafka.js
+++ dist/ClientKafka.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var kafkajs_1 = require("kafkajs");
 var kafka = new kafkajs_1.Kafka({
-    clientId: 'payment-server',
+    clientId: 'prove-server',
     brokers: ['localhost:9092'],
 });
 exports.default = kafka;
--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var producer_1 = __importDefault(require("./providers/producer"));
+var consumer_1 = __importDefault(require("./consumers/consumer"));
 setInterval(function () {
     producer_1.default();
 }, 1000);
-// getMessage();
+consumer_1.default();
